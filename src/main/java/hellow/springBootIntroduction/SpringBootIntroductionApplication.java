02c4b6be08e644ab654ecd9be3c4759b98bbe1c2
parkchanjong <7000cj@gmail.com>
2021-10-26 14:36:47 +0900

    set up

---
+++ src/main/java/hellow/springBootIntroduction/SpringBootIntroductionApplication.java
@@ -0,0 +1,13 @@
+package hellow.springBootIntroduction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootIntroductionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootIntroductionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hellow/springBootIntroduction/SpringBootIntroductionApplicationTests.java
@@ -0,0 +1,13 @@
+package hellow.springBootIntroduction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootIntroductionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
