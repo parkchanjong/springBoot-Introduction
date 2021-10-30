14189a35d49ba9f5ca72bb3e23918fcc37d2bfc1
parkchanjong <7000cj@gmail.com>
2021-10-31 00:57:42 +0900

    회원조회 기능 추가

--- src/main/java/hellow/springBootIntroduction/controller/MemberController.java
+++ src/main/java/hellow/springBootIntroduction/controller/MemberController.java
@@ -4,8 +4,11 @@ import hellow.springBootIntroduction.domain.Member;
 import hellow.springBootIntroduction.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -24,8 +27,16 @@ public class MemberController {
     @PostMapping(value = "/members/new")
     public String create(MemberForm form) {
         Member member = new Member();
+        System.out.println(form.getName());
         member.setName(form.getName());
         memberService.join(member);
         return "redirect:/";
+    }
+
+    @GetMapping(value = "/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
