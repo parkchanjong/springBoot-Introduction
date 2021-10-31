230d119c0ab979c8461e0794f0fcb661d82e0fa8
parkchanjong <7000cj@gmail.com>
2021-11-01 00:33:45 +0900

    add SpringDataJPA

---
+++ src/main/java/hellow/springBootIntroduction/repository/SpringDataJpaMemberRepository.java
@@ -0,0 +1,10 @@
+package hellow.springBootIntroduction.repository;
+
+import hellow.springBootIntroduction.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
+    Optional<Member> findByName(String name);
+}
